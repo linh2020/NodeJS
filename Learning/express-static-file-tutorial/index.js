949837191294bf8df046738b102e7f14dc7b058e
Linh Huynh <cdev.linh@gmail.com>
2024-04-11 02:12:59 -0700

    Creating Your Express Server

---
+++ Learning/express-static-file-tutorial/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () =>
+  console.log(`Express server is listening on port ${PORT}`)
+);
