72683252004e1d88b1c5c7bbb26770dc1115eda8
Linh Huynh <cdev.linh@gmail.com>
2024-04-11 02:22:03 -0700

    Express - Serving Your Static Files

--- Learning/express-static-file-tutorial/index.js
+++ Learning/express-static-file-tutorial/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+// Express provides a built-in method to serve your static files:
+app.use(express.static("public"));
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
