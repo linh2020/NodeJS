e77690b5a01f73173d4d089d450442f30afdb061
Linh Huynh <cdev.linh@gmail.com>
2024-04-12 01:24:16 -0700

    Simple JSON API with Express

--- Learning/express-static-file-tutorial/index.js
+++ Learning/express-static-file-tutorial/index.js
@@ -3,12 +3,11 @@ const app = express();
 const PORT = 5000;
 
 // Express provides a built-in method to serve your static files:
-app.use(express.static("public"));
-
-
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  console.log(req.url);
+  res.send("Hello NodeJS");
 });
 
 app.listen(PORT, () =>
