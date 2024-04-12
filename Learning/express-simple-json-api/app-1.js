e8487d543cf56dae09009ba4212ed7a9c8797132
Linh Huynh <cdev.linh@gmail.com>
2024-04-12 02:21:18 -0700

    Params, Query String - Setup

--- Learning/express-simple-json-api/app-1.js
+++ Learning/express-simple-json-api/app-1.js
@@ -6,17 +6,7 @@ const { products } = require("./data");
 
 app.get("/", (req, res) => {
   //   res.json([{ name: "linh" }, { name: "david" }]);
-
-  res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
-});
-
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-
-  res.json(newProducts);
+  res.json(products);
 });
 
 app.listen(PORT, () =>
