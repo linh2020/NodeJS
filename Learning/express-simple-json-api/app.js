0992e67af5f297f22a13f630d207aa67adef7d71
Linh Huynh <cdev.linh@gmail.com>
2024-04-17 22:49:21 -0700

    Express with Query String

--- Learning/express-simple-json-api/app.js
+++ Learning/express-simple-json-api/app.js
@@ -6,7 +6,6 @@ const { products } = require("./data");
 
 app.get("/", (req, res) => {
   //   res.json([{ name: "linh" }, { name: "david" }]);
-
   res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
 });
 
@@ -21,7 +20,7 @@ app.get("/api/products", (req, res) => {
 
 // /:productID
 app.get("/api/products/:productID", (req, res) => {
-  //   console.log(req.params);
+  console.log(req.params);
   const { productID } = req.params;
 
   const singleProduct = products.find(
@@ -38,6 +37,39 @@ app.get("/api/products/:productID", (req, res) => {
 app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
   console.log(req.params);
   res.send("Multiple properties in params.");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  // Customize products return
+  sortedProducts = sortedProducts.map((product) => ({
+    id: product.id,
+    name: product.name,
+    price: product.price,
+    company: product.company,
+  }));
+
+  // console.log(sortedProducts);
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) =>
+      product.name.includes(search)
+    );
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1)
+    res.status(200).json({ success: true, data: [] });
+  else res.status(200).json(sortedProducts);
+
+  // res.send("Hello world!");
 });
 
 app.listen(PORT, () =>
