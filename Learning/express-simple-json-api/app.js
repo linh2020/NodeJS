5d0b39f90e0d90d853fa4c0a56cee85b1108192b
Linh Huynh <cdev.linh@gmail.com>
2024-04-18 02:14:33 -0700

    Express - App.use() - Mounting a middleware function for router

--- Learning/express-simple-json-api/app.js
+++ Learning/express-simple-json-api/app.js
@@ -2,20 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
+const logger = require("./logger.js");
+app.use(logger);
 
-app.get("/", logger, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Home");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(PORT, () =>
