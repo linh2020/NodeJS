b4c99e3d2f4d39121c40baede74f983176ff68e8
Linh Huynh <cdev.linh@gmail.com>
2024-04-23 10:34:10 -0700

    Methods - POST (Javascript Example) - express.json() - json format

--- Learning/express-example-2/app.js
+++ Learning/express-example-2/app.js
@@ -10,8 +10,23 @@ app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 
+// parse json data
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).json({ success: true, msg: "Form Submitted Successfully" });
 });
 
 app.post("/login", (req, res) => {
