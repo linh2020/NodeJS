da6b7ebbee3eb84a7ff121815ae61c5334d36cc8
Linh Huynh <cdev.linh@gmail.com>
2024-04-23 02:18:51 -0700

    Methods - POST (Form Example) with Form /login, post method.

--- Learning/express-example-2/app.js
+++ Learning/express-example-2/app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const app = express();
 const PORT = 5000;
 
-const app = express();
+let { people } = require("./data");
+
+// static assets
+app.use(express.static("./methods-public"));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome to ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
+});
 
 app.listen(PORT, () =>
   console.log(`Express server is listening on port ${{ PORT }}`)
