34a200447e059ed3901b4fd46ac65cac6f9e9fc6
linh2020 <cdev.linh@gmail.com>
2024-04-29 23:06:00 -0700

    mongoose - crud - post

--- mongoose-demo-1/app.js
+++ mongoose-demo-1/app.js
@@ -3,18 +3,38 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
 
+const Product = require("./models/product.model.js");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    console.log(product);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// mongodb+srv://[username:password@]host[/[defaultauthdb][?options]]
+// cluster: backenddb
+// database-name: CdevPhoneStore
+// mongodb+srv://cdevlinh:<password>@backenddb.yqclkwv.mongodb.net/[database-name]?retryWrites=true&w=majority&appName=BackendDB
 mongoose
   .connect(
-    "mongodb+srv://cdevlinh:pGVWDLIIvtpxoTJL@cluster0.kqgwaxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://cdevlinh:pGVWDLIIvtpxoTJL@backenddb.yqclkwv.mongodb.net/CdevPhoneStore?retryWrites=true&w=majority&appName=BackendDB"
   )
   .then(() => {
     console.log("Connected to database!");
+
     app.listen(PORT, () =>
       console.log(`Express server is listening on port ${PORT}`)
     );
   })
   .catch(() => console.log("Connection Failed!"));
-
-app.get("/", (req, res) => {
-  res.send("Test");
-});
