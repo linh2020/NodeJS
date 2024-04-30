fdfa74a9d7bea4e28a1cf689ef0ae216c190b506
linh2020 <cdev.linh@gmail.com>
2024-04-30 00:55:29 -0700

    Delete a product using Model.findByIdAndDelete(id)

--- mongoose-demo-1/app.js
+++ mongoose-demo-1/app.js
@@ -63,6 +63,22 @@ app.put("/api/products/:id", async (req, res) => {
   }
 });
 
+// Delete a product
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // mongodb+srv://[username:password@]host[/[defaultauthdb][?options]]
 // cluster: backenddb
 // database-name: CdevPhoneStore
