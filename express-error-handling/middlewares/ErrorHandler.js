314e9437e82ef63b2f6e4b10312624f446c4b091
linh2020 <cdev.linh@gmail.com>
2024-05-13 22:52:02 -0700

    Updated .

--- express-error-handling/middlewares/ErrorHandler.js
+++ express-error-handling/middlewares/ErrorHandler.js
@@ -1,12 +1,4 @@
-const ErrorHandler = (err, req, res, next) => {
-  console.log("Middleware Error Handling");
-  const errStatus = err.statusCode || 500;
-  const errMsg = err.message || "Something went wrong";
+const ErrorHandler = (err, req, res, next) =>
+  res.status(400).json({ message: err.message });
 
-  res.status(errStatus).json({
-    success: false,
-    status: errStatus,
-    message: errMsg,
-    stack: process.env.NODE_ENV === "development" ? err.stack : {},
-  });
-};
+module.exports = ErrorHandler;
